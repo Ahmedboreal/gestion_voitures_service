23b7dfb3082942b7c6ba8cd1c5a339316b4f41a3
ahmed <ahmed@email.com>
2026-03-08 23:22:28 -0400

    creation classe voiture

---
+++ voiture.py
@@ -0,0 +1,11 @@
+class Voiture:
+
+    def __init__(self, matricule, annee, marque, kilometrage):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+
+    def afficherInformations(self):
+        print("Matricule:", self.matricule)
+        print("Marque:", self.marque)
