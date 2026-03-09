9cb5b716ce900908bb310837d712efe5d2168c67
ahmed <ahmed@email.com>
2026-03-08 23:51:07 -0400

    ajout fichier main

---
+++ main.py
@@ -0,0 +1,8 @@
+from employe import Employe
+from voiture import Voiture
+
+v1 = Voiture("A123", 2020, "Toyota", 50000)
+e1 = Employe("P111", "Ali", "Karim")
+
+e1.afficherInformations()
+v1.afficherInformations()
