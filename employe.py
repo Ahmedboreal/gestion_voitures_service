542093f9e2d5c1fca6996ffd3add445a161531c6
ahmed <ahmed@email.com>
2026-03-08 23:48:21 -0400

    ajout constructeur Employe

--- employe.py
+++ employe.py
@@ -1,2 +1,7 @@
 class Employe:
-    pass
+
+    def __init__(self, numeroPermis, nom, prenom):
+        self.numeroPermis = numeroPermis
+        self.nom = nom
+        self.prenom = prenom
+        self.voitureService = None
